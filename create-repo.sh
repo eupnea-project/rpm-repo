a7ffe7d2cfe141c42d8dbeb643f072ed69193298
Apacelus <46828271+Apacelus@users.noreply.github.com>
2022-12-04 21:53:14 +0000

    Update create-repo.sh

--- create-repo.sh
+++ create-repo.sh
@@ -2,7 +2,6 @@
 # This script creates an rpm repository layout in the current directory.
 set -e
 
-# Prepare system
 # Set gpg rpm signing key id
 echo "%_signature gpg
 %_gpg_name C2FD94D27193AF9D55E351C529CA5218233BC283" >~/.rpmmacros
@@ -12,14 +11,14 @@ echo "$private_key" >gpg-private-key.gpg
 # import gpg private key
 gpg --import gpg-private-key.gpg
 
-# Create repodata folder
-createrepo_c .
-
 # Copy built rpms to the current directory
 cp ~/rpmbuild/RPMS/*/*.rpm .
 
 # Sign rpms
 rpm --addsign ./*.rpm
+
+# Create repodata folder
+createrepo_c .
 
 # Sign repodata
 gpg --detach-sign --armor repodata/repomd.xml
