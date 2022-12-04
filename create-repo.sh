0743aa7475d05606fb92f98b573b9f870c2292ca
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-12-04 22:18:46 +0100

    Fix rpms path

--- create-repo.sh
+++ create-repo.sh
@@ -6,13 +6,16 @@ set -e
 # Set gpg rpm signing key id
 echo "%_signature gpg
 %_gpg_name C2FD94D27193AF9D55E351C529CA5218233BC283" > ./.rpmmacros
+
 # Dump gpg private key into a file
 echo "$private_key" > gpg-private-key.gpg
+
 # Create repodata folder
 createrepo_c .
 
+
 # Copy built rpms to the current directory
-cp ./rpmbuild/RPMS/*/*.rpm .
+cp ~/rpmbuild/RPMS/*/*.rpm .
 
 # Sign rpms
 rpm --addsign ./*.rpm
